d4582037ed8f72aa32110bf5c248bd59780dd311
Tomaz Setina <elektrotomi@windowslive.com>
2016-11-12 09:23:25 +0100

    Add project files.

---
+++ IRC/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="IRC.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>..:: Spletni klepet - 63130239 ::..</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:ListBox ID="LB_Pogovor" runat="server" Height="424px" Width="419px"></asp:ListBox>
+&nbsp;
+        <asp:ListBox ID="LB_Uporabniki" runat="server" Height="427px" Width="223px"></asp:ListBox>
+        <br />
+        <asp:TextBox ID="TB_Sporocilo" runat="server" Height="75px" Width="417px"></asp:TextBox>
+&nbsp;<asp:Button ID="B_Poslji" runat="server" CausesValidation="true" ValidationGroup="posiljanje" Height="78px" OnClick="B_Poslji_Click" Text="Pošlji" Width="108px" />
+&nbsp;<asp:Button ID="B_Osvezi" runat="server" Height="78px" OnClick="B_Osvezi_Click" Text="Refreš" Width="108px" /> 
+        <br />
+        <asp:RequiredFieldValidator 
+            ID="RequiredFieldValidator1" 
+            runat="server" 
+            ControlToValidate="TB_Sporocilo" 
+            ErrorMessage="Prazno vnosno polje. Vnesite sporocilo."
+            ValidationGroup="posiljanje"></asp:RequiredFieldValidator>
+    </div>
+    </form>
+</body>
+</html>
---
+++ IRC/Default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace IRC
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Application["Users"] == null )
+            {
+                Application["Users"] = new ArrayList();
+            }
+
+            if (Application["Messages"] == null)
+            {
+                Application["Messages"] = new ArrayList();
+            }
+
+            if (! IsPostBack)
+            {
+                Session["User"] = Session.SessionID;
+                LB_Pogovor.Items.Clear();
+                LB_Uporabniki.Items.Clear();
+
+                if (! ((ArrayList) Application["Users"]).Contains((string) Session["User"]))
+                {
+                    ((ArrayList) Application["Users"]).Add((string) Session["User"]);
+                }
+
+                for (int a = 0; a < ((ArrayList) Application["Users"]).Count; a++)
+                {       
+                    LB_Uporabniki.Items.Add((string) ((ArrayList)Application["Users"])[a]);  
+                }
+
+                for (int b = 0; b < ((ArrayList) Application["Messages"]).Count; b++)
+                {
+                    LB_Pogovor.Items.Add((string) ((ArrayList) Application["Messages"])[b]);
+                }
+            }
+        }
+
+        protected void B_Poslji_Click(object sender, EventArgs e)
+        {
+            LB_Pogovor.Items.Clear();
+            string userId = (string) Session["User"];
+            string message = TB_Sporocilo.Text;
+
+            if (Session["User"] != null)
+            {
+                ((ArrayList)Application["Messages"]).Add(userId.Substring(0, 3) + ": " + message);
+            }
+
+            TB_Sporocilo.Text = "";
+            for (int a = 0; a < ((ArrayList)Application["Messages"]).Count; a++)
+            {
+                LB_Pogovor.Items.Add((string)((ArrayList)Application["Messages"])[a]);
+            }
+        }
+
+        protected void B_Osvezi_Click(object sender, EventArgs e)
+        {
+            LB_Pogovor.Items.Clear();
+            LB_Uporabniki.Items.Clear();
+
+            for (int a = 0; a < ((ArrayList)Application["Users"]).Count; a++)
+            {
+                LB_Uporabniki.Items.Add((string)((ArrayList)Application["Users"])[a]);
+            }
+
+            for (int b = 0; b < ((ArrayList)Application["Messages"]).Count; b++)
+            {
+                LB_Pogovor.Items.Add((string)((ArrayList)Application["Messages"])[b]);
+            }
+        }
+    }
+}
